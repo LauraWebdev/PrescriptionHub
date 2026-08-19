b7283c8c968b78f61ed06ddac574b9153135a44a
Laura Sofia Heimann <hello@laura.media>
2026-08-19 12:33:37 +0200

    Initial commit

---
+++ app/src/main/java/media/laura/prescriptionhub/MainActivity.kt
@@ -0,0 +1,90 @@
+package media.laura.prescriptionhub
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import media.laura.prescriptionhub.ui.theme.PrescriptionHubTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PrescriptionHubTheme {
+                PrescriptionHubApp()
+            }
+        }
+    }
+}
+
+@PreviewScreenSizes
+@Composable
+fun PrescriptionHubApp() {
+    var currentDestination by rememberSaveable { mutableStateOf(AppDestinations.TODAY) }
+
+    NavigationSuiteScaffold(
+        navigationSuiteItems = {
+            AppDestinations.entries.forEach {
+                item(
+                    icon = {
+                        Icon(
+                            painterResource(it.icon),
+                            contentDescription = it.label
+                        )
+                    },
+                    label = { Text(it.label) },
+                    selected = it == currentDestination,
+                    onClick = { currentDestination = it }
+                )
+            }
+        }
+    ) {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )
+        }
+    }
+}
+
+enum class AppDestinations(
+    val label: String,
+    val icon: Int,
+) {
+    TODAY("Today", R.drawable.ic_home),
+    CALENDAR("Calendar", R.drawable.ic_favorite),
+    PRESCRIPTION("Prescription", R.drawable.ic_account_box),
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PrescriptionHubTheme {
+        Greeting("Android")
+    }
+}
